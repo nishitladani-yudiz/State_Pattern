652cc3c4f0250e86be3d14a8a96579bd5e0d360a
nishitladani-yudiz <nishit.l@yudiz.in>
2021-10-08 18:50:47 +0530

    Working on the target following script

---
+++ StatePattern/Assets/State/Scripts/TargetAssigner.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace Examples.State
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class TargetAssigner : MonoBehaviour
+    {
+        public event Action<Vector3> NewTargetAcquired = delegate { };
+
+        [SerializeField] AudioClip _newTargetSound = null;
+        [SerializeField] GameObject _targetIndicatorPrefab = null;
+        GameObject _targetIndicator;
+
+        Camera _camera = null;
+        RaycastHit _hitInfo;
+
+        private void Awake()
+        {
+            // get references
+            _camera = Camera.main;
+            // setup the target indicator visual and hide it
+            _targetIndicator = Instantiate(_targetIndicatorPrefab, _hitInfo.point, Quaternion.identity);
+            _targetIndicator.SetActive(false);
+        }
+
+        void Update()
+        {
+            //set new target
+            if (Input.GetMouseButtonDown(0))
+            {
+                GetNewMouseHit(_camera);
+                SetNewTargetPoint(_hitInfo.point);
+            }
+        }
+
+        public void GetNewMouseHit(Camera camera)
+        {
+            // get a ray hit point from camera click location
+            Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out _hitInfo, Mathf.Infinity))
+            {
+                Debug.Log("Ray hit: " + _hitInfo.transform.name
+                    + " at coordinates: " + _hitInfo.point);
+            }
+        }
+
+        public void SetNewTargetPoint(Vector3 position)
+        {
+            // handle the target visual
+            _targetIndicator.SetActive(true);
+            _targetIndicator.transform.position = position;
+            // feedback
+            AudioSource.PlayClipAtPoint(_newTargetSound, _camera.transform.position);
+            // send out notification
+            NewTargetAcquired.Invoke(position);
+        }
+    }
+}
+
